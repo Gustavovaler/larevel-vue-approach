74d1e306f274a59770a4a510d291d93434b0d79b
gustavo <gustavodevaler@gmail.com>
2020-05-06 03:12:32 -0300

    B

---
+++ resources/js/components/Contenido.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+
+    <form action="">
+        <input type="text" class="form-control">
+        <input type="text" class="form-control">
+        <input type="text" class="form-control">
+        <button class="btn btn-primary">Enviar</button>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
